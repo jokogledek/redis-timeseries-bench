71aff48af82b34fbb2559202eb89f64f78b2b45c
M Azwar Nurrosat <azwar.nrst@gmail.com>
2022-03-30 17:51:32 +0700

    cek commit

--- go.mod
+++ go.mod
@@ -3,5 +3,6 @@ module github.com/ujunglangit-id/redis-timeseries-bench
 go 1.17
 
 require (
-	github.com/rs/zerolog v1.26.1 // indirect
+	github.com/rs/zerolog v1.26.1
+	github.com/rueian/rueidis v0.0.40
 )
--- pkg/model/config/init.go
+++ pkg/model/config/init.go
@@ -3,11 +3,10 @@ package config
 import (
 	"fmt"
 	"github.com/rs/zerolog/log"
-	"net"
 	"os"
 )
 
-func InitConfig(versionCheck bool) (cfg *Config, err error) {
+func InitConfig() (cfg *Config, err error) {
 	log.Infof("config load [OK]")
 	cfg = &Config{}
 	err = cfg.readFile()
@@ -15,47 +14,6 @@ func InitConfig(versionCheck bool) (cfg *Config, err error) {
 		return
 	}
 
-	if versionCheck {
-		log.Infof("skipping secret config")
-		return
-	}
-
-	err = cfg.readSecret()
-	if err != nil {
-		return
-	}
-
-	return
-}
-
-func (cfg *Config) readSecret() (err error) {
-	var (
-		sec *SecretParam
-		f   *os.File
-	)
-	log.Infof("[config][readSecret] load secret param from %s", fmt.Sprintf(`%ssecret.param.yml`, cfg.Server.SecretPath))
-	f, err = os.Open(fmt.Sprintf(`%ssecret.param.yml`, cfg.Server.SecretPath))
-	if err != nil {
-		return
-	}
-	decoder := yaml.NewDecoder(f)
-	err = decoder.Decode(&sec)
-	if err != nil {
-		return
-	}
-
-	//set db auth param
-	for k, v := range cfg.Database {
-		v.DBAuth = DBAuthParam{
-			MasterAuthUname: sec.DbAuth[k].MasterAuthUname,
-			MasterAuthPass:  sec.DbAuth[k].MasterAuthPass,
-			SlaveAuthUname:  sec.DbAuth[k].SlaveAuthUname,
-			SlaveAuthPass:   sec.DbAuth[k].SlaveAuthPass,
-		}
-	}
-
-	cfg.Mail = sec.Mail
-	log.Info("[config][readSecret] Load Secret Param success")
 	return
 }
 
@@ -71,11 +29,11 @@ func (cfg *Config) readFile() (err error) {
 	log.Infof("[cfg.Environment ADALAH] %s", cfg.Environment)
 
 	path := []string{
-		"/etc/api-config",
-		"files/etc/api-config",
-		"./files/etc/api-config",
-		"../files/etc/api-config",
-		"../../files/etc/api-config",
+		"/config/api-config",
+		"files/config/api-config",
+		"./files/config/api-config",
+		"../files/config/api-config",
+		"../../files/config/api-config",
 	}
 
 	for _, val := range path {
@@ -94,41 +52,4 @@ func (cfg *Config) readFile() (err error) {
 	cfg.setLocalAddress()
 	log.Infof("[config][ReadConfig] Config load success, running on \"%s\".", cfg.Environment)
 	return
-}
-
-func (cfg *Config) GetLocalAddress() string {
-	return cfg.Server.LocalAddress
-}
-
-func (cfg *Config) setLocalAddress() {
-	// Once Set , we do not want to Reset it again
-	if cfg.Server.LocalAddress != "" {
-		return
-	}
-
-	localIP, err := cfg.getOutboundIP()
-	if err != nil {
-		log.Errorf("[config][SetLocalIP] Error Fetching IP. Error : %+v", err)
-		return
-	}
-
-	cfg.Server.LocalAddress = localIP
-
-}
-
-func (cfg *Config) getOutboundIP() (string, error) {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		return "", err
-	}
-
-	for _, a := range addrs {
-		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				return ipnet.IP.String(), nil
-			}
-		}
-	}
-
-	return "", nil
 }
